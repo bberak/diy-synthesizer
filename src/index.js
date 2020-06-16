aa688c87d4a55965fda1fcea0e22c68efc9ae79d
bberak <bberak@gmail.com>
2020-06-16 13:36:38 +1000

    Syntax fix

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const aggregateStates = aggregate(
 	([s1, s2, s3]) => {
 		if (s1 == "off" && s2 == "off" && s3 == "off") {
 			exec(`say turning off`);
-			if (process.env.NODE_ENV == "dev");
+			if (process.env.NODE_ENV == "dev")
 				process.exit();
 			else 
 				exec(`sudo shutdown -h now`);
