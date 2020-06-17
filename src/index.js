d139edf76ea24ecd7d73ef2d5190d9447557b403
bberak <bberak@gmail.com>
2020-06-17 15:34:10 +1000

    Reverting exit routine

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const synth = require("./synthesizer");
 const button = require("./pup-button");
 const knob = require("./rotary-encoder");
 const { stateMachine, aggregate } = require("./state-machine");
-const { exec, execSync } = require("child_process");
+const { exec } = require("child_process");
 
 exec(`sudo amixer set PCM 100%`);
 exec(`say turning on`);
@@ -23,13 +23,14 @@ const aggregateStates = aggregate(
 	[leftKnobStates, middleKnobStates, rightKnobStates],
 	([s1, s2, s3]) => {
 		if (s1 == "off" && s2 == "off" && s3 == "off") {
-			try {
-				execSync(`say turning off`);
-			} catch (e) {
-			} finally {
-				if (process.env.NODE_ENV == "dev") process.exit();
-				else exec(`sudo shutdown -h now`);
-			}
+			//-- Unfortunately the sentence below cannot be heard when turning off..
+			//-- And using execSync causes some un-catchable errors.
+			exec(`say turning off`);
+			
+			if (process.env.NODE_ENV == "dev")
+				process.exit();
+			else 
+				exec(`sudo shutdown -h now`);
 		}
 	}
 );
