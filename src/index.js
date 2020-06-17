01f5ec7c3afb404d45762c379096fdc950eed38e
bberak <bberak@gmail.com>
2020-06-18 00:24:59 +1000

    Reverting to what works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const synth = require("./synthesizer");
 const button = require("./pup-button");
 const knob = require("./rotary-encoder");
 const { stateMachine, aggregate } = require("./state-machine");
-const { exec, spawnSync } = require("child_process");
+const { exec } = require("child_process");
 
 exec(`sudo amixer set PCM 100%`);
 exec(`say turning on`);
@@ -23,8 +23,9 @@ const aggregateStates = aggregate(
 	[leftKnobStates, middleKnobStates, rightKnobStates],
 	([s1, s2, s3]) => {
 		if (s1 == "off" && s2 == "off" && s3 == "off") {
-			exec("say turning off");
-			spawnSync("sleep 2");
+			//-- Unfortunately the sentence below cannot be heard when turning off..
+			//-- And using execSync causes some un-catchable errors.
+			exec(`say turning off`);
 			
 			if (process.env.NODE_ENV == "dev")
 				process.exit();
